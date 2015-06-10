ee9ffc1183ea69d5447a2174762762fa21653be9
NieneB <niene.boeijen@gmail.com>
2015-06-10 12:36:19 +0200

    stijl

--- script.js
+++ script.js
@@ -1,41 +1,31 @@
 var map;
+var geojsonLayer;
+var broek = {
+    tekst : "<p>Een broek is een laaggelegen gebied dat nat blijft door opwellend grondwater, kwel, of is een langs een rivier of beek gelegen laag stuk land dat regelmatig overstroomt en s winters vaak langere tijd onder water staat. Een broek kan variëren van een moerassig stuk veengrond tot uitgestrekte uiterwaarden (kleigrond). Veel broeklanden zijn onbruikbaar voor agrarische doeleinden omdat ze te nat zijn. Sommige stukken broekland kunnen worden begraasd in de drogere perioden. Een sterk begroeid stuk broekland wordt broekbos genoemd. Hier kan bosbouw (grienden) plaatsvinden, hoewel nauwelijks machines kunnen worden ingezet omdat die zouden wegzinken in de drassige bodem. Het woord broek is verwant aan het Duitse Bruch (moeras) en het Engelse brook (beek) en is afgeleid van het Germaanse woord broka wat moeras betekent. Het is mogelijk dat dit Germaanse woord verwant is aan het Keltische woord brogilo, dat terugkomt in namen als Brielle en Bruil, en gebruikt wordt voor (vaak bosachtige) moerrassige terreinen. In deze betekenis is het woord broek onzijdig, het is dus het broek. Diverse plaatsen in Nederland en Vlaanderen zijn genoemd naar dit landschap, zie Broek (toponiem). Aan verscheidene toponiemen is het onzijdige woordgeslacht te zien, bijvoorbeeld Het Bossche Broek. Toch komt de variant De Broek wel voor: Het Bossche Broek wordt ook veelvuldig aangeduid als De Bossche Broek en verder is er De Broek, een buurtschap in de gemeente Veldhoven. Achternamen als Van den Broek of Vandenbroucke verwijzen rechtstreeks naar dit landschapstype, of naar het toponiem.</p>"
+};
+
 
 document.addEventListener("DOMContentLoaded", function(event) { 
   var tileUrl = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png';
-  map = L.map('map').setView([52.3808, 5.0918], 8); 
+  map = L.map('map').setView([52.3808, 4.6000], 8); 
   L.tileLayer(tileUrl).addTo(map);
 });
 
-
-function loadName(name){
-  var concept = loadPlace(name);
-  var read = new XMLHttpRequest();
-          read.open('GET', "info/"+name+'.txt', false);
-          read.send();
-          var displayName = parseInt(read.responseText)
-  document.getElementById("text").textContent= displayName;
-  console.log(concept);
+function loadName(place){
+  document.getElementById("text").innerHTML = place;
+  
+  loadPlace(place);
+  
 };
 
 function loadPlace(place){
   var url = "https://api.histograph.io/search?name=" + "*"+place + "&type=hg:Place";
+  
   d3.json(url, function(err, concepts){
     console.log("Calling api.histograph.io for places with: '" + place + "':");
-    console.log(concepts.features[0].properties.pits[0].name)
-    L.geoJson(concepts).addTo(map).bindPopup(place).openPopup();
-    //draw layer on map
-    
+    console.log(concepts.features[0].properties.pits[0].name);
+    geojsonLayer = L.geoJson(concepts);
+    geojsonLayer.addTo(map).bindPopup(place);
   }); 
 };
-  
-// var greenIcon = L.icon({
-//     iconUrl: 'leaf-green.png',
-//     shadowUrl: 'leaf-shadow.png',
-//
-//     iconSize:     [38, 95], // size of the icon
-//     shadowSize:   [50, 64], // size of the shadow
-//     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-//     shadowAnchor: [4, 62],  // the same for the shadow
-//     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-// });
 
